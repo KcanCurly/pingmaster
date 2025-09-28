af06c94e46367b02c570d67a3aaa93dd19081d8f
kcancurly <karadumankcan@gmail.com>
2025-09-28 22:12:54 +0300

    u

--- pingmaster/pingmain.py
+++ pingmaster/pingmain.py
@@ -1,31 +1,9 @@
 import argparse
 from concurrent.futures import ThreadPoolExecutor
-from scapy.all import send
-from scapy.layers.inet import IP
 from pingmaster.tcp import send as send_tcp
+from pingmaster.udp import send as send_udp
 import time
 from datetime import datetime
-
-def send_safe(target):
-    ip_packet = IP(dst=target)
-    send(ip_packet, verbose=False)
-
-def send_broken_version_packet(target, version = 1):
-    ip_packet = IP(dst=target, version=version) / b"PM_VERSION"
-    send(ip_packet, verbose=False)
-
-def send_packets_with_all_tos(target):
-    for tos_value in range(256):  # TOS values range from 0 to 255
-        # Create an IP packet with the current TOS value
-
-        ip_packet = IP(dst=target, tos=tos_value) / (bytes(f"PM_TOS_{tos_value}", "utf-8"))
-        
-        # Send the packet
-        send(ip_packet)
-
-def send_do_not_fragment(target):
-    ip_packet = IP(dst=target, flags="DF") / b"PM_DF"
-    send(ip_packet, verbose=False)
 
 MAX_PORT = 65536
 
@@ -36,12 +14,6 @@ def main():
     args = parser.parse_args()
     target = args.target
     threads = args.threads
-
-    # Create a raw IP packet
-    # send_safe(target)
-    # send_broken_version_packet(target)
-    # send_packets_with_all_tos(target)
-    # send_do_not_fragment(target)
 
     now = datetime.now()
     start = time.time()
@@ -59,3 +31,20 @@ def main():
     end = time.time()
     minutes, seconds = map(int,divmod(end - start, 60))
     print(f"TCP SYN PING took around {minutes} minutes and {seconds} seconds")
+
+    now = datetime.now()
+    start = time.time()
+    print("Starting UDP PING")
+    print("Date:", now)
+    print("===================")
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        # TCP S flag test
+        for i in range(1, MAX_PORT):
+            executor.submit(send_udp, target, i, "pingmaster")
+    now = datetime.now()
+    print("===================")
+    print("ENDING UDP PING")
+    print("Date:", now)
+    end = time.time()
+    minutes, seconds = map(int,divmod(end - start, 60))
+    print(f"UDP PING took around {minutes} minutes and {seconds} seconds")
--- pingmaster/udp.py
+++ pingmaster/udp.py
@@ -1,61 +1,15 @@
-from scapy.all import sniff, send, Raw
+from scapy.all import Raw
+from scapy.all import send as scapy_send
 from scapy.layers.inet import IP, UDP
-import argparse
-import socket
-import netifaces as ni
 
-target_payload = b""
-send_payload = b""
-received = False
+def send(ip, port, data):
+    b = data.encode("utf-8", errors="replace")
+    # Craft TCP packet
+    packet = (
+        IP(dst=ip, id=34443) /
+        UDP(dport=port, sport=44444) /
+        Raw(load=b)
+    )
 
-def handle_packet(packet):
-    if UDP in packet and bytes(packet[UDP].payload) in target_payload:
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        src_port = packet[UDP].sport
-        dst_port = packet[UDP].dport
-        print(f"{src_ip}:{src_port} → {dst_ip}:{dst_port}")
-
-        response = IP(dst=src_ip) / UDP(sport=dst_port, dport=src_port) / Raw(load=send_payload)
-        send(response, verbose=False)
-
-
-
-
-def server():
-    parser = argparse.ArgumentParser(description="Listen to UDP packets using Scapy.")
-    parser.add_argument("-i", "--interface", default="eth0", help="Interface to listen to")
-    parser.add_argument("-l", "--listen-data", default="Hello from pingmaster", help="Payload/message to listen to")
-    parser.add_argument("-d", "--data", default="Hello from pingmaster server", help="Payload/message to send")
-    args = parser.parse_args()
-    global target_payload, send_payload
-    target_payload = bytes(args.listen_data, "utf-8")
-    send_payload = bytes(args.data, "utf-8")
-    print(f"Server started, going to send '{args.data}' for udp two-way connection testing")
-
-    sniff(iface=args.interface, filter=f"udp and dst host {ni.ifaddresses(args.interface)[ni.AF_INET][0]['addr']}", prn=handle_packet, store=False)
-
-def client():
-    parser = argparse.ArgumentParser(description="Send UDP packets using Scapy.")
-    parser.add_argument("-t", "--target", required=True, help="Destination IP address")
-    parser.add_argument("-s", "--start-port", type=int, required=True, help="Start of destination port range")
-    parser.add_argument("-e", "--end-port", type=int, required=True, help="End of destination port range")
-    parser.add_argument("-d", "--data", default="Hello from pingmaster", help="Payload/message to send")
-    parser.add_argument("-w", "--wait-for", default=3, help="How much to wait for an answer back (Default = 3)")
-
-    args = parser.parse_args()
-
-    for dst_port in range(args.start_port, args.end_port + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(args.wait_for)
-
-        # Send to server
-        sock.sendto(bytes(args.data, "utf-8"), (args.target, dst_port))
-        print(f"[>] Sent: {args.data}")
-
-        # Wait for reply
-        try:
-            data, addr = sock.recvfrom(4096)
-            print(f"[<] Reply from {addr}: {data.decode(errors='replace')}")
-        except socket.timeout:
-            print("[!] No response — possibly one-way UDP communication.")
+    # Send packet
+    scapy_send(packet, verbose=False)
