7a6f996c1556b33ac92522ff0db55bb9f8b0359a
kcancurly <karadumankcan@gmail.com>
2025-09-29 12:11:21 +0300

    u

--- pingmaster/pingmain.py
+++ pingmaster/pingmain.py
@@ -46,60 +46,60 @@ def test_text_post(name):
     minutes, seconds = map(int,divmod(end - start, 60))
     print(f"{name} took around {minutes} minutes and {seconds} seconds")
 
-def test_tcp(target, threads):
+def test_tcp(target, threads, data):
     test_text_pre("TCP PING")
 
     with ThreadPoolExecutor(max_workers=threads) as executor:
         # TCP S flag test
         for i in range(1, MAX_PORT):
-            executor.submit(send_tcp, target, i, "pingmaster", "S")
+            executor.submit(send_tcp, target, i, data, "S")
 
     test_text_post("TCP PING")
 
-def test_udp(target, threads):
+def test_udp(target, threads, data):
     test_text_pre("UDP PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
         # TCP S flag test
         for i in range(1, MAX_PORT):
-            executor.submit(send_udp, target, i, "pingmaster")
+            executor.submit(send_udp, target, i, data)
     test_text_post("UDP PING")
 
-def test_icmp(target, threads):
+def test_icmp(target, threads, data):
     test_text_pre("ICMP PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
         # TCP S flag test
         for i in range(1, MAX_ICMP_TYPES):
-            executor.submit(send_icmp, target, i, "pingmaster")
+            executor.submit(send_icmp, target, i, data)
     test_text_post("ICMP PING")
 
-def test_ah(target, threads):
+def test_ah(target, threads, data):
     test_text_pre("AH PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.submit(send_ah, target, "pingmaster")
+        executor.submit(send_ah, target, data)
     test_text_post("AH PING")
 
-def test_esp(target, threads):
+def test_esp(target, threads, data):
     test_text_pre("ESP PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.submit(send_esp, target, "pingmaster")
+        executor.submit(send_esp, target, data)
     test_text_post("ESP PING")
 
-def test_gre(target, threads):
+def test_gre(target, threads, data):
     test_text_pre("GRE PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.submit(send_gre, target, "pingmaster")
+        executor.submit(send_gre, target, data)
     test_text_post("GRE PING")
 
-def test_igmp(target, threads):
+def test_igmp(target, threads, data):
     test_text_pre("IGMP PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.submit(send_igmp, target, "pingmaster")
+        executor.submit(send_igmp, target, data)
     test_text_post("IGMP PING")
 
-def test_igmp(target, threads):
+def test_pim(target, threads, data):
     test_text_pre("PIM PING")
     with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.submit(send_igmp, target, "pingmaster")
+        executor.submit(send_pim, target, data)
     test_text_post("PIM PING")
 
 def main():
@@ -107,27 +107,29 @@ def main():
     parser.add_argument("target", help="Target IP address or hostname")
     parser.add_argument("-t", "--threads", type=int, default=10, help="Amount of threads. (Default: 10)")
     parser.add_argument("-m", "--method", required=False, type= PingTypes, choices=list(PingTypes))
+    parser.add_argument("-d", "--data", type=str, default="pingmaster", help="Data to send. (Default: pingmaster)")
     args = parser.parse_args()
     target = args.target
     threads = args.threads
+    data = args.data
 
     if args.method:
         if args.method == PingTypes.TCP:
-            test_tcp(target, threads)
+            test_tcp(target, threads, data)
         elif args.method == PingTypes.UDP:
-            test_udp(target, threads)
+            test_udp(target, threads, data)
         elif args.method == PingTypes.ICMP:
-            test_icmp(target, threads)
+            test_icmp(target, threads, data)
         elif args.method == PingTypes.AH:
-            test_ah(target, threads)
+            test_ah(target, threads, data)
         elif args.method == PingTypes.ESP:
-            test_esp(target, threads)
+            test_esp(target, threads, data)
         elif args.method == PingTypes.GRE:
-            test_gre(target, threads)
+            test_gre(target, threads, data)
         elif args.method == PingTypes.IGMP:
-            test_igmp(target, threads)
+            test_igmp(target, threads, data)
         elif args.method == PingTypes.PIM:
-            test_igmp(target, threads)
+            test_pim(target, threads, data)
 
     else:
         test_tcp(target, threads)
