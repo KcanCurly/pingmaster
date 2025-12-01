d34c93f59e8fdd3d3666f282558cac2b4b0d19ce
kcancurly <karadumankcan@gmail.com>
2025-12-01 21:20:20 +0300

    u

--- pingmaster/client.py
+++ pingmaster/client.py
@@ -23,19 +23,20 @@ def main():
     if os.geteuid() != 0:
         print("Run as root, exiting")
 
-    for port in range(0, 65536):
-        try:
-            random_data = bytes(''.join(random.choice(chars) for _ in range(10)), "utf-8")
-            packet = (
-                IP(dst=args.ipv4_target, id=34443) /
-                TCP(dport=port, sport=44444, flags="S") /
-                Raw(load=random_data)
-            )
-            print(f"> [{packet[IP].dst}] | [{packet[Raw].load}]")
+    try:
+        for port in range(0, 65536):
+            
+                random_data = bytes(''.join(random.choice(chars) for _ in range(10)), "utf-8")
+                packet = (
+                    IP(dst=args.ipv4_target, id=34443) /
+                    TCP(dport=port, sport=44444, flags="S") /
+                    Raw(load=random_data)
+                )
+                print(f"> [{packet[IP].dst}] | [{packet[Raw].load}]")
 
-            incoming_packet = sr1(packet, verbose=False, timeout=args.timeout)
-            if incoming_packet:
-                print(f"< [{incoming_packet[IP].src}] | [{incoming_packet[Raw].load}]")
-        except KeyboardInterrupt:
-            sys.exit(1)
+                incoming_packet = sr1(packet, verbose=False, timeout=args.timeout)
+                if incoming_packet:
+                    print(f"< [{incoming_packet[IP].src}] | [{incoming_packet[Raw].load}]")
+    except KeyboardInterrupt:
+        sys.exit(1)
 
